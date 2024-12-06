885c807bd73a40dfc69512d8852553a39d5620c0
yago-gouveia <150040567+yago-gouveia@users.noreply.github.com>
2024-12-06 10:49:38 -0300

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // Certifique-se de incluir todos os arquivos onde o Tailwind será utilizado
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
