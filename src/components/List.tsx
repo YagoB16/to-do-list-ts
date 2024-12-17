8103cdebefcd9b39d9187e2ed48fff4d024c67da
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2024-12-16 21:19:01 -0300

    tentativa de ajuste em layout

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,13 +8,14 @@ export function List() {
 
     return (
         <>
-            <div className="w-92p rounded-lg flex justify-center items-center py-4  gap-2 bg-[#262626]">
-                <input className="rounded-xl" type="checkbox" name="" id="" />
-                <p className="px-3 ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem aliquid dolor porro libero nostrum soluta quibusdam iure magni, itaque vitae? Saepe at suscipit nulla itaque eligendi beatae alias nemo a.
-                    Eligendi modi officia voluptatibus expedita</p>
-                <button
-                    title='Deletar task'>
-                    <Trash size={24} />
+            <div className="w-92p h-20 rounded-lg flex justify-between items-center p-2 bg-[#262626]  first:mt-4 last:mb-8">
+                <div className='w-6 h-6 -mt-4 ml-4 '>
+                    <input className=" " type="checkbox" name="" id="" />
+                </div>
+                <p className="pl-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercita Los polos hermanos</p>
+                <button title='Deletar task' className='w-6 h-6 -mt-4 pr-4 pb-8'>
+                    <Trash size={24}
+                        className="text-gray-500 hover:[color:red]" />
                 </button>
             </div>
         </>
