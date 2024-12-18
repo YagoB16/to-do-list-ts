6f5feb039dfeb85bf4d144b76a8afd6826a0d56e
yago-gouveia <150040567+yago-gouveia@users.noreply.github.com>
2024-12-18 11:50:20 -0300

    criação de componente EmptyCell, - Separação de listagem de tasks e menu sem task

---
+++ src/components/EmptyCell.tsx
@@ -0,0 +1,21 @@
+import Clipboard from '../assets/Clipboard.svg'
+
+export function EmptyCell() {
+  const today = new Date()
+
+  return (
+    <>
+      <div className="w-92p flex flex-col items-center gap-4 rounded-lg bg-transparent border-t-2 border-[#80808048]  ">
+        <div className="pt-14">
+          <img src={Clipboard} alt="" />
+        </div>
+        <div className="text-center text-[#808080]">
+          <p className="text-inter-bold">Você ainda não adicionou uma tarefa</p>
+          <p className="text-inter">
+            Crie uma tarefa para hoje {today.toLocaleDateString()}
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
